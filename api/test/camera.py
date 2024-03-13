fe1f2c3ae833b3771db9d186b420e92a858fd685
Jason Ng <jng40@stuy.edu>
2024-03-13 17:54:13 -0400

    added images

--- api/test/camera.py
+++ api/test/camera.py
@@ -1,25 +1,20 @@
-import cv2 as cv
-cam = cv.VideoCapture(cam_port) 
+import cv2
+cam_port = 0
+cam = cv2.VideoCapture(cam_port) 
   
-# reading the input using the camera 
-result, image = cam.read() 
-  
-# If image will detected without any error,  
-# show result 
-if result: 
-  
-    # showing result, it take frame name and image  
-    # output 
-    cv.imshow("GeeksForGeeks", image) 
-  
-    # saving image in local storage 
-    cv.imwrite("GeeksForGeeks.png", image) 
-  
-    # If keyboard interrupt occurs, destroy image  
-    # window 
-    cv.waitKey(0) 
-    cv.destroyWindow("GeeksForGeeks") 
-  
-# If captured image is corrupted, moving to else part 
-else: 
-    print("No image detected. Please! try again") 
+
+if not cam.isOpened():
+    raise IOError("Cannot open webcam")
+
+while True:
+    ret, frame = cam.read()
+    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+    cv2.imshow('Food', frame)
+
+    c = cv2.waitKey(1)
+    if c == 27:
+        cv2.imshow("Food", frame) 
+        cv2.imwrite("Food.jpg", frame) 
+        break
+cv2.destroyAllWindows()
+cam.release()
